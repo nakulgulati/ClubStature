3cdb5d25a6055c5ea556d9b59f25addeb03720ce
Nakul Gulati <nakgulati@gmail.com>
2013-07-08 20:22:42 +0530

    20:22

--- includes/constants.php
+++ includes/constants.php
@@ -5,3 +5,5 @@ define("DB_SERVER","localhost");
 define("DB_USER","root");
 define("DB_PASS","");
 define("DB_NAME","ratemyclub");
+
+?>
--- signup.php
+++ signup.php
@@ -8,6 +8,49 @@
   echo $nav;
 ?>
 
+<?php
+//Form processing
+$errors = array();
+if(isset($_POST['submit'])){
+    //Form submitted
+    
+    
+    
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = sha1($_POST['password']);
+    $verifyPassword = sha1($_POST['verifyPassword']);
+    
+    if(!ctype_alnum($username)){
+	array_push($errors,"Username cannot contain special characters.");
+    }
+    
+    if(strlen($password)>6){
+	if($password != $verifyPassword){
+	    array_push($errors,"Passwords don't match.");
+	}
+    }
+    else{
+	array_push($errors,"Password should be minimum 6 characters.");
+    }
+    
+    if(count($errors)==0){
+	$query = "INSERT INTO `users`(`username`, `email`, `hashedPass`) VALUES ('{$username}','{$email}','{$password}');";
+	
+	if(mysql_query($query,$connection)){
+	    $message = "Sign Up success.";
+	}
+	else{
+	    $message = "Sign Up failed.";
+	}
+    }
+}	
+else{
+    //Form not submitted
+}
+?>
+
+
 <div class="wrapper">
 	<div class="container">
 		<div class = "row">
@@ -16,15 +59,15 @@
 			</div>
 			<div class = "span6">
 			<!--form area-->
-				<form class="form-horizontal" method="post" action="signupProcess.php">
+				<form class="form-horizontal" method="post" action="signup.php">
 					<legend>
 						Sign Up
 						<span class="pull-right">(or <a href="login.php">Sign in</a>)</span>
 					</legend>
 					<div class="control-group">
-						<label class="control-label" for="fname">Name</label>
+						<label class="control-label" for="username">Username</label>
 						<div class="controls">
-							<input type="text" id="fname" name="name" placeholder="John Doe" required>
+							<input type="text" id="username" name="username" placeholder="Select a username" required>
 						</div>
 					</div>
 					<div class="control-group">
@@ -34,26 +77,36 @@
 						</div>
 					</div>
 					<div class="control-group">
-						<label class="control-label" for="verifyEmail">Verify Email</label>
-						<div class="controls">
-							<input type="email" id="verifyEmail" name="verifyEmail" placeholder="johndoe@example.com" required>
-						</div>
-					</div>
-					<div class="control-group">
 						<label class="control-label" for="password">Password</label>
 						<div class="controls">
 							<input type="password" id="password" name="password" placeholder="******" required>
 							<span class="help-inline">Min Length 6</span>
 						</div>
 					</div>
+										<div class="control-group">
+						<label class="control-label" for="verifyPassword">Verify Password</label>
+						<div class="controls">
+							<input type="password" id="verifyPassword" name="verifyPassword" placeholder="******" required>
+						</div>
+					</div>
 					<div class="control-group">
 						<div class="controls">
 							<label class="checkbox"></label>
-							<input type="checkbox"> I accept the Terms and Conditions.
-							<button type="submit" class="btn btn-info pull-right">Create Account</button>
+							<input type="checkbox" required> I accept the Terms and Conditions.
+							<button type="submit" name ="submit" class="btn btn-info pull-right">Create Account</button>
 						</div>
 					</div>
 				</form>
+				<?php
+				
+				if(count($errors)){
+				    print_r($errors);
+				}
+				
+				if(isset($_POST['submit'])){
+				    echo $message;
+				}
+				?>
 			</div>
 		</div>
 	</div>
