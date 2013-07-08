603f044ab89f891f8072b067902f2d0e504b66ff
Nakul Gulati <nakgulati@gmail.com>
2013-07-09 02:07:49 +0530

    minor changes

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php require_once("includes/sessions.php"); ?>
 <?php require_once("includes/connection.php"); ?>
 <?php require_once("includes/functions.php"); ?>
 <?php include("includes/header.php"); ?>
