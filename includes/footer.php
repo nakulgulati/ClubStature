4e5d7184368a176f9f66d98d1524f87f87411d9a
Amit Kalay <amitkalay@yahoo.com>
2013-07-11 12:10:50 +0530

    got rid of crap in my branch

--- includes/footer.php
+++
@@ -1,7 +0,0 @@
-<?php mysql_close($connection); ?>
-		<!--jquery-->
-		<script src="http://code.jquery.com/jquery.js"></script>
-		<script src="js/bootstrap.js"></script>
-    </body>
-</html>
-<?php ob_end_flush(); ?>
