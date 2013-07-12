fb63d2554bf0da82d94b6ca08e7946c015860d24
Sudip Guha <sudipguha29@gmail.com>
2013-07-13 01:16:12 +0530

    Update clubcreated.php

--- clubcreated.php
+++ clubcreated.php
@@ -1,7 +1,7 @@
 <div class="container">
   <h1>Your Club Has Been Created!</h1>
   <div class="well">
-    <p>Your Club Request has been successfully received and is being processed...</p>
+    <p><b>Your Club Request has been successfully received and is being processed...</b></p>
   </div>
   <div class="row"></div>
   <div class="btn-group">
