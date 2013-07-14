1237a3677604202efaed0d104d45960a3ef19a97
Amit Kalay <amitkalay@yahoo.com>
2013-07-14 14:50:45 +0530

    Need to rename variables and connect with navbar

--- SearchClubs.php
+++ SearchClubs.php
@@ -1,69 +1,60 @@
-<!doctype html>
-<html>
-  
-  <head>
-    <title>BrowsClubs</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta content="text/html; charset=UTF-8" http-equiv="Content-Type">
-    <link rel="stylesheet" href="https://app.divshot.com/css/bootstrap.css">
-    <link rel="stylesheet" href="https://app.divshot.com/css/bootstrap-responsive.css">
-    <script src="https://app.divshot.com/js/jquery.min.js"></script>
-    <script src="https://app.divshot.com/js/bootstrap.min.js"></script>
-  </head>
-  
-  <body>
-    <div class="container">
-      <h1>Browse Clubs...</h1>
-    </div>
-    <div class="container">
-      <br>
-      <br>
-      <br>
-      <p>
-        <b>Look for clubs by category and other criteria...</b> 
-      </p>
-      <br>
-      <br>
-      <form class="form-inline">
-        <p>Or Search for a club using keyword</p>
-        <input type="btn" value="Search">
-        <input type="btn" class="btn input-mini" name="searchText" value="Search"> 
-      </form>
-      <div class="container">
-        <div class="row">
-          <div class="span4">
-            <h3>Category</h3>
-            <label class="checkbox" for="checkbox">
-              <input type="checkbox" value="true" id="checkbox" name="checkbox"> Checkboxes can be toggled on and off</label>
-            <label class="checkbox" for="checkbox">
-              <input type="checkbox" value="true" id="checkbox" name="checkbox"> Checkboxes can be toggled on and off</label>
-            <label class="checkbox" for="checkbox">
-              <input type="checkbox" value="true" id="checkbox" name="checkbox"> Checkboxes can be toggled on and off</label>
-          </div>
-          <div class="span4">
-            <h3>College</h3>
-            <label class="checkbox" for="checkbox">
-              <input type="checkbox" value="true" id="checkbox" name="checkbox"> Checkboxes can be toggled on and off</label>
-            <label class="checkbox" for="checkbox">
-              <input type="checkbox" value="true" id="checkbox" name="checkbox"> Checkboxes can be toggled on and off</label>
-            <label class="checkbox" for="checkbox">
-              <input type="checkbox" value="true" id="checkbox" name="checkbox"> Checkboxes can be toggled on and off</label>
-            <label class="checkbox" for="checkbox">
-              <input type="checkbox" value="true" id="checkbox" name="checkbox"> Checkboxes can be toggled on and off</label>
-          </div>
-          <div class="span4">
-            <h3>Size</h3>
-            <label class="checkbox" for="checkbox">
-              <input type="checkbox" value="true" id="checkbox" name="checkbox"> Checkboxes can be toggled on and off</label>
-            <label class="checkbox" for="checkbox">
-              <input type="checkbox" value="true" id="checkbox" name="checkbox"> Checkboxes can be toggled on and off</label>
-            <label class="checkbox" for="checkbox">
-              <input type="checkbox" value="true" id="checkbox" name="checkbox"> Checkboxes can be toggled on and off</label>
-          </div>
-        </div>
+<title>BrowseClubs</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta content="text/html; charset=UTF-8" http-equiv="Content-Type">
+<link rel="stylesheet" href="https://app.divshot.com/css/bootstrap.css">
+<link rel="stylesheet" href="https://app.divshot.com/css/bootstrap-responsive.css">
+<div class="container">
+  <h1>Browse Clubs...</h1>
+</div>
+<div class="container">
+  <br>
+  <br>
+  <br>
+  <p>
+    <b></b> 
+  </p>
+  <h1>
+    <b>Look for clubs by category and other criteria... </b> 
+  </h1>
+  <p></p>
+  <br>
+  <br>
+  <form class="form-inline">
+    <p>Or Search for a club using keyword</p>
+    <input type="btn" value="Search">
+    <input type="btn" class="btn input-mini" name="searchText" value="Search"> 
+  </form>
+  <div class="container">
+    <div class="row">
+      <div class="span4">
+        <h3>College or School</h3>
+        <label class="checkbox" for="checkbox">
+          <input type="checkbox" value="true" id="checkbox" name="Umich"> University of Michigan</label>
+        <label class="checkbox" for="checkbox">
+          <input type="checkbox" value="true" id="checkbox" name="NIIT"> NIIT Neemrana</label>
+        <label class="checkbox" for="checkbox">
+          <input type="checkbox" value="true" id="checkbox" name="Berkeley"> UC-Berkeley</label>
       </div>
-      <div class="control-group"></div>
+      <div class="pull-right span4">
+        <h3>Category</h3>
+        <label class="checkbox" for="checkbox">
+          <input type="checkbox" value="true" id="checkbox" name="sports"> Sports and Recreation</label>
+        <label class="checkbox" for="checkbox">
+          <input type="checkbox" value="true" id="checkbox" name="checkbox"> Art, Music and Culture</label>
+        <label class="checkbox" for="checkbox">
+          <input type="checkbox" value="true" id="checkbox" name="checkbox"> Academic/Professional</label>
+        <label class="checkbox" for="checkbox">
+          <input type="checkbox" value="true" id="checkbox" name="checkbox"> Community Service/Volunteering</label>
+        <label class="checkbox" for="checkbox">
+          <input type="checkbox" value="true" id="checkbox" name="checkbox"> Governance</label>
+        <label class="checkbox" for="checkbox">
+          <input type="checkbox" value="true" id="checkbox" name="checkbox"> Greek Life</label>
+        <label class="checkbox" for="checkbox">
+          <input type="checkbox" value="true" id="checkbox" name="checkbox"> Science and Technology</label>
+        <label class="checkbox" for="checkbox">
+          <input type="checkbox" value="true" id="checkbox" name="lifestyle"> Life Style</label>
+      </div>
     </div>
-  </body>
-
-</html>
+  </div>
+  <div class="control-group"></div>
+</div>
