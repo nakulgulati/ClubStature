aa82eab4c786e9d18c8e183ab418bc6da044457e
Amit Kalay <amitkalay@yahoo.com>
2013-07-18 18:01:57 +0530

    Interface for the addClubs page

--- addClub.php
+++ addClub.php
@@ -8,45 +8,53 @@
   $nav = printNav(true);
   echo $nav;
 ?>
-<title> addClub.php </title>
+
+<?php
+//Prints nav bar
+  $nav = printNav(true);
+  echo $nav;
+?>
+
+<title>addClub.php</title>
 <div class="wrapper">
-      <div class="container">
-        <div class="well">
-          <h1>Create a club page</h1>
-        </div>
-        <div class="row">
-          <div class="span5">
-            <p>This page will help you to set up your club page.</p>
-            <p>Please fill all the fields. Those marked by * are necessary.</p>
-          </div>
-          <div class="span8">
-            <div class="container-fluid">
-              <p>
-                <br> 
-              </p>
-              <label>Name of the Club</label>
-              <input type="text" class="input-medium">
-              <label>Category</label>
-              <br>
-              <input type="text" class="input-medium">
-              <label> College </label>
-              <input type="text" class="input-medium">
-              <label>Link to the Club Page</label>
-              <input type="text" class="input-medium">
-              <p>Please give a brief description of your club...</p>
-              <div class="row"></div>
-              <div class="drag-mask" data-ds-form="textarea" style="width: 904px; height: 114px;">
-                <textarea style="margin: 0px -322px 10px 0px; width: 904px; height: 114px;"
-                class="input-block-level"></textarea>
-              </div>
-            </div>
-          </div>
-        </div>
+  <div class="container">
+    <div class="well">
+      <h1>Create a club page</h1>
+    </div>
+    <div class="row">
+      <div class="span5">
+        <p>This page will help you to set up your club page.</p>
+        <p>Please fill all the fields. Those marked by * are necessary.</p>
       </div>
-      <div class="form-actions">
-        <button type="submit" class="btn btn-primary">Submit</button>
-        <input type="reset" class="btn" value="Reset"> 
+      <div class="span8">
+        <div class="container-fluid">
+          <p>
+            <br> 
+          </p>
+          <label>Name of the Club</label>
+          <input type="text" class="input-medium">
+          <label>Category</label>
+          <br>
+          <input type="text" class="input-medium">
+          <label>College</label>
+          <input type="text" class="input-medium">
+          <label>Link to the Club Page</label>
+          <input type="text" class="input-medium">
+          <label>Insert your club icon/picture </label>
+          <input type="hidden" name="MAX_FILE_SIZE" value="500" />
+          <input type="file" name = "picture">
+          <p>Please give a brief description of your club...</p>
+          <div class="row"></div>
+          <textarea style="margin: 0px -322px 10px 0px; width: 904px; height: 114px;"
+          class="input-block-level"></textarea>
+        </div>
       </div>
     </div>
+  </div>
+  <div class="form-actions">
+    <button type="submit" class="btn btn-primary">Submit</button>
+    <input type="reset" class="btn" value="Reset"> 
+  </div>
 </div>
+
 <?php include("includes/footer.php"); ?>
