f52e690e9575946235f2e03fa8de04313c5674af
Nakul Gulati <nakgulati@gmail.com>
2013-07-21 01:23:55 +0530

    Titles

--- includes/header.php
+++ includes/header.php
@@ -1,14 +1,31 @@
 <?php ob_start();
-date_default_timezone_set("Asia/Calcutta");
+	date_default_timezone_set("Asia/Calcutta");
+?>
+<?php
+    $title = "";
+    $self = $_SERVER['PHP_SELF'];
+    
+    if(strpos($self,"index.php")){
+	$title = "";
+    }
+    elseif(strpos($self,"about.php")){
+	$title = " - About";
+    }
+    elseif(strpos($self,"login.php")){
+	$title = " - Login";
+    }
+    elseif(strpos($self,"signup.php")){
+	$title = " - Sign Up";
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<title>Rate My Club</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-		<!--stylesheets-->
-		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-		<link rel="stylesheet" type="text/css" href="css/custom.css">
-	</head>	
-	<body>
+    <head>
+	<title>Rate My Club<?php echo $title; ?></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+	<!--stylesheets-->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/custom.css">
+    </head>	
+    <body>
