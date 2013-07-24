f7c412933cec7860f985415a9ac4de2c391a6776
Amit Kalay <amitkalay@yahoo.com>
2013-07-24 14:27:28 +0530

    a fileloader that PROBABLY works now

--- fileloader.php
+++ fileloader.php
@@ -1,55 +1,46 @@
 <?php
-$show=false;
-$message="";
-if(isset($_POST['submit']))
-{
-    echo "hi";
-    $show=true;
-    if ($_FILES["file"]["error"] > 0)
-    {
-        $message.="Error: " . $_FILES["file"]["error"] . "<br>";
-    }
-    else
-    {
-        $allowedExts = array("gif", "jpeg", "jpg", "png","JPEG"."GIF","JPG","PNG");
-        $temp = explode(".", $_FILES["file"]["name"]);
-        $extension = end($temp);
-        if ((($_FILES["file"]["type"] == "image/gif")
-        || ($_FILES["file"]["type"] == "image/jpeg")
-        || ($_FILES["file"]["type"] == "image/jpg")
-        || ($_FILES["file"]["type"] == "image/pjpeg")
-        || ($_FILES["file"]["type"] == "image/x-png")
-        || ($_FILES["file"]["type"] == "image/png"))
-        && in_array($extension, $allowedExts))
-        {
-          
-            $message.="Upload: " . $_FILES["file"]["name"] . "<br>";
-            $message.="Type: " . $_FILES["file"]["type"] . "<br>";
-            $message.="Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-            $message.="Stored in: " . $_FILES["file"]["tmp_name"];
-            move_uploaded_file($_FILES["file"]["tmp_name"],
-            "upload/" . $_FILES["file"]["name"]);
-            $message.="Stored in: " . "upload/" . $_FILES["file"]["name"];
-        }
-        else
-        {
-          echo "Invalid file";
-        }
-    }
-}
 
+if(isset($_POST['submit'])){
+$allowedExts = array("gif", "jpeg", "jpg", "png", "PNG", "JPEG", "GIF", "JPG");
+$temp = explode(".", $_FILES["file"]["name"]);
+$extension = end($temp);  
+
+	if ((($_FILES["file"]["type"] == "image/gif")
+	|| ($_FILES["file"]["type"] == "image/jpeg")
+	|| ($_FILES["file"]["type"] == "image/jpg")
+	|| ($_FILES["file"]["type"] == "image/pjpeg")
+	|| ($_FILES["file"]["type"] == "image/x-png")
+	|| ($_FILES["file"]["type"] == "image/png"))
+	&& ($_FILES["file"]["size"] < 1000000) //file size less than 1000 kB or 1 MB
+	&& in_array($extension, $allowedExts)) {
+		if($_FILES["file"]["error"] > 0){
+			echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+		}
+		else {
+			echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+			echo "Type: " . $_FILES["file"]["type"] . "<br>";
+			echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+			echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
+
+			if (file_exists("images/" . $_FILES["file"]["name"])){
+				echo $_FILES["file"]["name"] . " already exists. ";
+			}
+			else{
+				move_uploaded_file($_FILES["file"]["tmp_name"], "images/".$_FILES["file"]["name"]);
+				echo "Stored in: " . "images/" . $_FILES["file"]["name"];
+				}
+		}
+	}
+	else{
+		echo "Invalid file";
+	}
+
+}
 ?>
 <html>
-    <form name="filelaod" action="fileloader.php" method="POST"  enctype="multipart/form-data" required>
-        <input type="file" name="file" id="file" required/>
+    <form name="fileload" method="POST"  enctype="multipart/form-data" required>
+        <input type="file" name="file" id="file">
         <input type="submit" name="submit" value="submit"/>
     </form>
     <br/>
-    
-    <?php
-    if($show)
-    {
-        echo $message;
-    }
-    ?>
 </html>
