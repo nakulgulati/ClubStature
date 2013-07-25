3f632fe86880feb82ed2c29b7bb7d74b71827e7d
Nakul Gulati <nakgulati@gmail.com>
2013-07-26 03:50:02 +0530

    header

--- includes/header.php
+++ includes/header.php
@@ -21,7 +21,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-	<title>Rate My Club<?php echo $title; ?></title>
+	<title><?php echo NAME; ?><?php echo $title; ?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
     
 	<!--stylesheets-->
