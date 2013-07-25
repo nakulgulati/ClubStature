782cf9722a20edce48ec36992d38d8d85902d0c1
Amit Kalay <amitkalay@yahoo.com>
2013-07-25 11:41:48 +0530

    mail testing needed

--- template.php
+++ template.php
@@ -10,21 +10,39 @@
 ?>
 
 
+
 <div class="wrapper">
   <div class="container">
     <div class="hero-unit hidden-phone">
       <!--body content here-->
+	  
+	  <?php
+		if(isset($_GET["submitEmail"])){
+		echo "dawg, check your yahoo inbox if you got something.";
+		$to = "amitkalay@yahoo.com";
+		$from = "amitkal@umich.edu";
+		$message = "Yay! The message went through!!";
+		$subject = "testmail";
+		$headers = "From: " . $from;
+		mail($to,$subject,$message,$headers);
+		echo "<br>";
+		echo mail($to,$subject,$message,$headers);
+		}
+		?>
     </div>
-    <!--Dropdown Stuff-->
+    <!--Dropdown Stuff
                             <div class="btn-group pull-right">
                             <h3 class="user dopdown-toggle" data-toggle="dropdown">Username</h3>
                             <ul class="dropdown-menu">
-                            <li><a href="passchange.php> Change Password </a> </li>
+                            <li><a href="passchange.php"> Change Password </a> </li>
                             <li><a href="logout.php">Log Out</a></li>
                             </ul>
                             </div>
-              <br><br><br>              
-                            S
+              <br><br><br>  -->
+	<form method = "get">
+	Submit an email?
+	<input type = "submit" name="submitEmail">
+	</form>
   </div>
 </div>
             
