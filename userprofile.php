f6d0c0f9f8205940905926f8f36612f248430823
Amit Kalay <amitkalay@yahoo.com>
2013-07-27 13:23:17 +0530

    styling the user profile page

--- userprofile.php
+++ userprofile.php
@@ -12,25 +12,31 @@
 <head>
   <title>User Profile</title>
 </head>
+<style>
+body
+{
+background-color:#d0e4fe;
+}
+</style>
 
 <div class="wrapper">
   <div class="container">
     <div class="hero-unit hidden-phone">
       <!--body content here-->
-      <h1 class="text-center">profile information</h1>
+      <h1 class="text-center">Account Settings</h1>
       <?php
       $username=$_SESSION['username'];
       $result=mysql_query("SELECT* FROM users WHERE username='{$username}'");
       $row=mysql_fetch_array($result);
       $email=$row['email'];
-      echo "<h5>your username</h5>";
+      echo "<h5>Username</h5>";
       echo "<p class=\"text-info\"> {$username} </p></br>";
       echo "<h5>your email id</h5>";
       echo "<p class=\"text-info\"> {$email} </p></br>";
       ?>
-      <a href="forgotPassword.php">forgot password</a>
-      <a href="changePassword.php">change password</a>
-      <a href="deleteAccount.php">delete account</a>
+      <a href="forgotPassword.php">Forgot password</a> <br>
+      <a href="changePassword.php">Change password</a> <br>
+      <a href="deleteAccount.php">Delete account</a> <br>
       <!--end content-->
     </div>
   </div>
