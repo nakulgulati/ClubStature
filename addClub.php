73e156ca5f7817b27a2c299ad513276e1c691ba9
Nakul Gulati <nakgulati@gmail.com>
2013-07-28 02:52:20 +0530

    File upload

--- addClub.php
+++ addClub.php
@@ -10,42 +10,6 @@
 ?>
 
 <?php
-    function uploadFile($fileName,$clubName){
-	
-	global $connection;
-	
-	$allowedExts = array("gif", "jpeg", "jpg", "png", "PNG", "JPEG", "GIF", "JPG");
-	$temp = explode(".", $fileName);
-	$extension = end($temp);
-	
-	$query = "SELECT COUNT(id) AS total FROM clubs";
-	$resultSet = mysql_query($query,$connection);
-	$row = mysql_fetch_array($resultSet);
-	$id = $row['total'];
-	$id++;
-	
-	$newName = $id."-".str_replace(" ","",$clubName);
-	
-	if ((($_FILES["file"]["type"] == "image/gif")
-	|| ($_FILES["file"]["type"] == "image/jpeg")
-	|| ($_FILES["file"]["type"] == "image/jpg")
-	|| ($_FILES["file"]["type"] == "image/pjpeg")
-	|| ($_FILES["file"]["type"] == "image/x-png")
-	|| ($_FILES["file"]["type"] == "image/png"))
-	&& ($_FILES["file"]["size"] < 1000000) //file size less than 1000 kB or 1 MB
-	&& in_array($extension, $allowedExts)) {
-		if($_FILES["file"]["error"] > 0){
-			echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
-		}
-		else {			
-			move_uploaded_file($_FILES["file"]["tmp_name"], "img/clubImages/".$newName);
-			return $newName;
-    		}
-	}
-    }
-?>
-
-<?php
 if(isset($_POST['addClub'])){
     $clubName=$_POST['clubName'];
     $college=$_POST['college'];
