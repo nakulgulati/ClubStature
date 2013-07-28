ee56c65b217c8a62c4759a3a5b5f749096399edc
Amit Kalay <amitkalay@yahoo.com>
2013-07-29 00:30:31 +0530

    Complete topList interface, more or less

--- topList.php
+++ topList.php
@@ -16,51 +16,7 @@
 
 <div style = "text-align: center;"><h1><u> Top Lists </u></h1> </div>
 <br> <br> <br>
-    <!-- Carousel
-    ================================================== -->
-    <div id="myCarousel" class="carousel slide">
-      <!-- Indicators -->
-      <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1"></li>
-        <li data-target="#myCarousel" data-slide-to="2"></li>
-      </ol>
-      <div class="carousel-inner">
-        <div class="item active">
-          <img data-src="holder.js/1500x500/auto/#777:#7a7a7a/text:First slide" alt="">
-          <div class="container">
-            <div class="carousel-caption">
-              <h1>Example headline.</h1>
-              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-              <p><a class="btn btn-large btn-primary" href="#">Sign up today</a></p>
-            </div>
-          </div>
-        </div>
-        <div class="item">
-          <img data-src="holder.js/1500x500/auto/#777:#7a7a7a/text:Second slide" alt="">
-          <div class="container">
-            <div class="carousel-caption">
-              <h1>Another example headline.</h1>
-              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-              <p><a class="btn btn-large btn-primary" href="#">Learn more</a></p>
-            </div>
-          </div>
-        </div>
-        <div class="item">
-          <img data-src="holder.js/1500x500/auto/#777:#7a7a7a/text:Third slide" alt="">
-          <div class="container">
-            <div class="carousel-caption">
-              <h1>One more for good measure.</h1>
-              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-              <p><a class="btn btn-large btn-primary" href="#">Browse gallery</a></p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <a class="left carousel-control" href="#myCarousel" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
-      <a class="right carousel-control" href="#myCarousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
-    </div><!-- /.carousel -->
-
+   
 
 
     <!-- Marketing messaging and featurettes
@@ -73,86 +29,65 @@
       <div class="row">
         <div class="col-lg-4">
           <img class="img-circle" data-src="holder.js/140x140">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-          <p><a class="btn btn-default" href="#">View details &raquo;</a></p>
+          <h2>Overall Rating</h2>
+          
+          <p> <?php printTopList("overallRating"); ?></p>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
           <img class="img-circle" data-src="holder.js/140x140">
-          <h2>Heading</h2>
-          <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-          <p><a class="btn btn-default" href="#">View details &raquo;</a></p>
+
+
+          <h2>Rigor</h2>
+          <p><?php printTopList("rigor"); ?></p>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
           <img class="img-circle" data-src="holder.js/140x140">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn btn-default" href="#">View details &raquo;</a></p>
+
+
+          <h2>Supportiveness</h2>
+          <p> <?php printTopList("cohesiveness"); ?> </p>
         </div><!-- /.col-lg-4 -->
       </div><!-- /.row -->
 
-
-      <!-- START THE FEATURETTES -->
-
       <hr class="featurette-divider">
 
-      <div class="featurette">
-        <img class="featurette-image img-circle pull-right" data-src="holder.js/512x512">
-        <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-      </div>
-
-      <hr class="featurette-divider">
-
-      <div class="featurette">
-        <img class="featurette-image img-circle pull-left" data-src="holder.js/512x512">
-        <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-      </div>
-
-      <hr class="featurette-divider">
-
-      <div class="featurette">
-        <img class="featurette-image img-circle pull-right" data-src="holder.js/512x512">
-        <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-      </div>
-
-      <hr class="featurette-divider">
-
-      <!-- /END THE FEATURETTES -->
-
-
-      <!-- FOOTER -->
-      <footer>
-        <p class="pull-right"><a href="#">Back to top</a></p>
-        <p>&copy; 2013 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
-      </footer>
+      <h2>Time Commitment</h2>
+          <p> <?php printTopList("scheduleFriendliness"); ?> </p>
+        </div><!-- /.col-lg-4 -->
+      </div><!-- /.row -->
 
     </div><!-- /.container -->
+    <br> <br>
+
+    <div style = "text-align: center;"> 
+    
+    <label>College</label>
+                  
+    <?php
+        global $connection;
+        $output = "<select name =\"college\">";
+        $query = "SELECT name FROM colleges ORDER BY name";
+        $output.= "<option></option> ";
+        //geting the list of colleges
+        $resultset = mysql_query($query, $connection);
+        //$resultSet = getData("colleges","name");
+        while($row = mysql_fetch_array($resultset)){
+    $output .= "<option>{$row['name']}</option>";
+    }
+    
+    $output .= " </select>";
+    echo $output;
+    ?> <input type = "submit" name = "submitPreference"> 
+    </div>
 
 
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="../assets/js/jquery.js"></script>
-    <script src="../dist/js/bootstrap.js"></script>
-    <script src="../assets/js/holder.js"></script>
-  </body>
-</html>
-
-
-    <div class="row-fluid">
+  <!--  <div class="row-fluid">
         <?php printTopList("overallRating"); ?> //the overall rating top list...
         
         <?php printTopList("rigor"); ?>
-    </div>
-    
-    <div class="row-fluid">
-        <?php printTopList("cohesiveness"); ?>
-        <?php printTopList("scheduleFriendliness"); ?>
-    </div>
+    </div>  -->
+  
   </div>
 </div>
 
