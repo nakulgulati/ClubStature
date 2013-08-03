ba50a8083c0ad17163f50468162aa4b4491d867c
Amit Kalay <amitkalay@yahoo.com>
2013-08-03 21:45:18 +0530

    Sudip's sweet about page

--- about.php
+++ about.php
@@ -3,17 +3,37 @@
 <?php require_once("includes/functions.php"); ?>
 <?php include("includes/header.php"); ?>
 
-
-
-<div class="wrapper">
+<div class="wrapper" style="background:
+    linear-gradient(27deg, #151515 5px, transparent 5px) 0 5px,
+    linear-gradient(207deg, #151515 5px, transparent 5px) 10px 0px,
+    linear-gradient(27deg, #222 5px, transparent 5px) 0px 10px,
+    linear-gradient(207deg, #222 5px, transparent 5px) 10px 5px,
+    linear-gradient(90deg, #1b1b1b 10px, transparent 10px),
+    linear-gradient(#1d1d1d 25%, #1a1a1a 25%, #1a1a1a 50%, transparent 50%, transparent 75%, #242424 75%, #242424);
+    background-color: #131313;
+    background-size: 20px 20px;">
     <?php
-    //Prints nav bar.
+    //Prints nav bar
       $nav = printNav(true);
       echo $nav;
     ?>
-    <div class="wrapper-content">
+    <div class="banner">
         <div class="container">
-            <h3>
+            <div class="row">
+                <p class="welcomeText">About&nbsp;</p>
+                <p class="welcomeText welcomeText-club">Club&nbsp;</p>
+                <p class="welcomeText welcomeText-stature">Stature</p>
+                <p class="subText">A new way to make the most of College.</p>
+            </div>
+        </div>
+    </div>
+    
+    <div class="wrapper-content-home">
+        <div class="container">
+           <div class="row">
+            <div class="col-lg-10">
+              <div class="well" style="font-size: 24px">
+                <b>
                 <p>
                 Looking for ways to make the most of your college or high school's extracurricular opportunities?
                 Well, look no further! Here at clubstature, we try to help you choose the organizations that are the right fit for you. 
@@ -23,95 +43,57 @@
                 We want to make sure you&#39;re well-informed before committing.
                 Happy hunting!
                 </p>
-            </h3>
-            
-             <a data-toggle="modal" href="#myModal" class="btn btn-primary btn-large">Contact Info</a>
-
-  <!-- Modal -->
-  <div class="modal" id="myModal">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-          <h4 class="modal-title">Got any suggestions or feedback?  Would you like to see a new feature?</h4>
+                </b>
+              </div>
+            </div>
+           </div>
         </div>
-        <div class="modal-body">
-         Simply shoot us an email to clubstature@gmail.com!
-        </div>
-      </div><!-- /.modal-content -->
-    </div><!-- /.modal-dialog -->
-  </div><!-- /.modal -->
-
+        <div class="banner">
+        <div class="container">
+            <div class="row">
+                <p class="welcomeText">Privacy Policy&nbsp;</p>
+                <p class="subText">We are not evil.*</p>
+            </div>
             
         </div>
+        </div>
+        <div class="container">
+          <div class="row">
+        <div class="well" style="font-size: 24px">
+          <b>
+          The only data we collect from you is your username and email address.  <br> <br>
+          You may choose to submit your real name and institution you attend.  <br> <br>
+          Although this is by no means required, it will improve the authenticity of your review.  <br>
+          </b>
+        </div>
+        </div>
+        </div>
+        
+        <em><h1 style="color: white;text-align: center">Creators</h1></em>
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-4">
+              <div class="well" style="">Nakul Gulati
+              <br/>
+                Nakul's data
+              </div>
+            </div>
+            <div class="col-lg-4">
+              <div class="well" style="">Amit Kalay
+              <br/>
+                Amit's data
+              </div>
+            </div>
+            <div class="col-lg-4">
+              <div class="well" style="">Sudip Guha<br/>
+                Sudip's data
+              </div>
+            </div>
+          </div>
+        </div>
+        <br/><br/><br/>
+        
     </div>
-  
-
-<div class="container">
-  <div class="hero-unit">
-    <br> 
-    
-	
-  </div>
 </div>
-
-<div style="text-align: center;">
-
-</div>
-<br>
-
-<!-- Button to trigger modal -->
-<p class = "text-center">
- 
-</p>
-
-<!-- End of button -->    
-
-<div style = "text-align: center;">   <h1> Privacy Policy </h1>  </div>
-<br> <hr>
-
-<h3> The only data we collect from you is your username and email address.  <br> <br>
-	You may choose to submit your real name and institution you attend.  <br> <br>
-	Although this is by no means required, it will improve the authenticity of your review.  <br> </h3>
-
-<br><br><br> <hr>
-
-<table class="table table-striped table-hover">
-  <thead>
-    <tr>
-      <th>#</th>
-      <th>Chief Contributors</th>
-      <th>Facebook Page</th>
-	  <th> Email </th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>1</td>
-      <td>Amit Kalay</td>
-      <td>
-        <a href="https://www.facebook.com/amit.kalay.9?fref=ts">Amit's page</a>
-      </td>
-	  <td> amitkal@umich.edu </td>
-    </tr>
-    
-	<tr>
-      <td>2</td>
-      <td>Nakul Gulati</td>
-      <td> <a href="https://www.facebook.com/nakul.gulati?fref=ts"> Nakul's page</a> </td>
-	  <td> nakul.behindthestrokes@gmail.com </td>
-	  
-    </tr>
-	
-    <tr>
-      <td>3</td>
-      <td>Sudip Guha</td>
-      <td> <a href="https://www.facebook.com/sudip.guha.524?fref=ts"> Sudip's page </a> </td>
-	  <td> sudip.guha29@gmail.com </td>
-    </tr>
-  </tbody>
-</table>
-</div>
-
 
 <?php include("includes/footer.php"); ?>
