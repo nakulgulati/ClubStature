7bfb0e3b4f905cd157c6670bd53c6a93d416f799
Sudip Guha <sudipguha29@gmail.com>
2013-08-04 21:20:13 +0530

    Code with function to add a college

---
+++ addcollege.php
@@ -0,0 +1,57 @@
+<?php require_once("includes/sessions.php"); ?>
+<?php require_once("includes/connection.php"); ?>
+<?php require_once("includes/functions.php"); ?>
+<?php include("includes/header.php"); ?>
+
+<?php
+    //method to add a college to the database
+    function addCollege($college)
+    {
+        global $connection;
+        $query="INSERT INTO colleges(name) VALUES('{$college}')";
+        if(mysql_query($query,$connection))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+?>
+
+<div class="wrapper">
+    <?php
+    //Prints nav bar
+      $nav = printNav(true);
+      echo $nav;
+    ?>
+    <div class="wrapper-content">
+      <div class="well">
+        <form method="get" action="addcollege.php">
+            <input type="text" name="college"/>
+            <input type="submit" name="submit"/>
+        </form>
+        <div>
+            <?php
+                if(isset($_GET['submit']))
+                {
+                    $college=$_GET['college'];
+                    echo $college;
+                    if(addCollege($college))
+                    {
+                        echo "success";
+                    }
+                    else
+                    {
+                        echo "failed";
+                    }
+                }
+            ?>
+        </div>
+      </div>
+    </div>
+</div>
+
+<?php include("includes/footer.php"); ?>
