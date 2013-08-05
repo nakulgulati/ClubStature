6f0a7544daabbfb2ba745d1cce0896a46294e927
Nakul Gulati <nakgulati@gmail.com>
2013-08-05 23:11:12 +0530

    Footer

--- includes/header.php
+++ includes/header.php
@@ -37,4 +37,14 @@
 			<link href='http://fonts.googleapis.com/css?family=Joti+One' rel='stylesheet' type='text/css'>
 	    
     </head>	
-    <body>	
+    <body>
+	<div id="fb-root"></div>
+	<script>
+		(function(d, s, id) {
+		  var js, fjs = d.getElementsByTagName(s)[0];
+		  if (d.getElementById(id)) return;
+		  js = d.createElement(s); js.id = id;
+		  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=627888060569403";
+		  fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
+	</script>
