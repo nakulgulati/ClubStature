237660b2f203aefe102b0406fe0d20d7dd1ef08d
Nakul Gulati <nakgulati@gmail.com>
2013-08-27 11:55:35 +0530

    Google Analytics

--- includes/header.php
+++ includes/header.php
@@ -26,7 +26,8 @@
 	<title><?php echo NAME; ?><?php echo $title; ?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		
+	
+	<!--favicon-->	
 	<link rel="icon" href="assets/images/favicon.ico" type="image/x-icon">
 	<link rel="shortcut icon" href="assets/images/favicon.ico" type="image/x-icon">
     
@@ -36,9 +37,20 @@
 	<link rel="stylesheet" type="text/css" href="css/custom.css">
 	<link rel="stylesheet" type="text/css" href="css/customFonts.css">
 	<link rel="stylesheet" type="text/css" href="css/typeahead.js-bootstrap.css">
-	<link href='http://fonts.googleapis.com/css?family=Racing+Sans+One' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Joti+One' rel='stylesheet' type='text/css'>
 	<link href="assets/social-buttons/css/zocial.css" rel='stylesheet' type='text/css'>
+		
+	<!--Google Analytics-->
+	<script>
+	    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	  
+	    ga('create', 'UA-43517157-1', 'clubstature.com');
+	    ga('send', 'pageview');
+      
+	</script>
 	    
     </head>	
     <body>
